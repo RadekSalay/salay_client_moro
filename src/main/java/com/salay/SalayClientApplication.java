e820d3a95be8cb4264cd34591865d05b1387890c
RadekSalay <radek.salay@gmail.com>
2016-11-06 19:10:23 +0100

    Start

---
+++ src/main/java/com/salay/SalayClientApplication.java
@@ -0,0 +1,12 @@
+package com.salay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalayClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalayClientApplication.class, args);
+	}
+}
